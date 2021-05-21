1794565cf46366876f5eef259d624c38432daf37
ravetam <rave.tam@gmail.com>
2021-05-21 13:04:33 +0800

    Update AlarmServer.py

--- AlarmServer.py
+++ AlarmServer.py
@@ -19,6 +19,10 @@ logging.basicConfig(level=logging.INFO)
 def on_connect(client, userdata, flags, rc):
     logging.info("Connected with result code "+str(rc))
 
+def on_publish(client,userdata,result):
+    logging.info("Data published")
+    pass
+    
 with open('config.yaml', 'r') as file:
     settings = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -29,9 +33,11 @@ mqtt_password = settings['MQTT_PASSWORD']
 
 client = mqtt.Client("cameras") 
 client.on_connect = on_connect
+client.on_publish = on_publish
 client.username_pw_set(username=mqtt_username,password=mqtt_password)
 logging.info("Connecting...")
 client.connect(mqtt_address, mqtt_port)
+client.loop_start()
 
 def GetIP(s):
     return inet_ntoa(struct.pack("<I", int(s, 16)))
@@ -46,13 +52,16 @@ while True:
         sleep(0.1)  # Just for recive whole packet
         data = conn.recv(len_data)
         conn.close()
-        reply = json.loads(data)
-        logging.info(datetime.now().strftime("[%Y-%m-%d %H:%M:%S]>>>"))
-        logging.info(head, version, session, sequence_number, msgid, len_data)
-        logging.info(json.dumps(reply, indent=4, sort_keys=True))
-        logging.info("<<<")
+        reply = data.decode('utf-8')
+        client.publish("cameras", reply)
+        
+        #reply = json.loads(data)
+        #logging.info(datetime.now().strftime("[%Y-%m-%d %H:%M:%S]>>>"))
+        #logging.info(head, version, session, sequence_number, msgid, len_data)
+        #logging.info(json.dumps(reply, indent=4, sort_keys=True))
+        #logging.info("<<<")
 
-        client.publish("cameras",json.dumps(reply))  
+        #client.publish("cameras",json.dumps(reply))  
 
     except (KeyboardInterrupt, SystemExit):
         break
